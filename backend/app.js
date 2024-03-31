16230c221f30f66ea1720312863d9063172dce79
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-04-01 01:13:21 +0200

    establish connection with db

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import productRouter from "./routes/products.js";
+import { connectDB } from "./config/connectDB.js";
 const app = express();
 dotenv.config({
   path: "backend/config/config.env",
@@ -8,8 +9,17 @@ dotenv.config({
 
 app.use("/api/v1", productRouter);
 
-app.listen(process.env.PORT, () => {
-  console.log(
-    `server is listinening on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`
-  );
-});
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `server is listinening on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ backend/config/connectDB.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  let uri = "";
+  if (process.env.NODE_ENV === "DEVELOPMENT") uri = process.env.MONGO_LOCAL_URI;
+  if (process.env.NODE_ENV === "PRODUCTION") uri = process.env.MONGO_URI;
+  const con = await mongoose.connect(uri);
+  console.log(`mongodb connected with host ${con.connection?.host}`);
+};
