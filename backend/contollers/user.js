95e9bf2b3b804c1f860653f7c334436129adce3e
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-04-13 15:10:29 +0200

    create register user controller

---
+++ backend/contollers/user.js
@@ -0,0 +1,7 @@
+import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
+import User from "../models/user.js";
+
+export const registerUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.create(req.body);
+  res.status(201).json({ user });
+});
