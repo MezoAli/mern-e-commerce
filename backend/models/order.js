761c51bf616a53d6e28a369b5f666a1f9d4624c8
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-04-16 11:27:21 +0200

    create order model

---
+++ backend/models/order.js
@@ -0,0 +1,99 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema(
+  {
+    shippingInfo: {
+      address: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+      zipCode: {
+        type: String,
+        required: true,
+      },
+      phoneNo: {
+        type: String,
+        required: true,
+      },
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    orderItems: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        image: {
+          type: String,
+          required: true,
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "Product",
+        },
+      },
+    ],
+    paymentMethod: {
+      type: String,
+      enum: {
+        values: ["COD", "Card"],
+        message: "please select a payment method",
+      },
+    },
+    paymentInfo: {
+      id: String,
+      status: String,
+    },
+    itemsPrice: {
+      type: Number,
+      required: true,
+    },
+    taxAmount: {
+      type: Number,
+      required: true,
+    },
+    shippingAmount: {
+      type: Number,
+      required: true,
+    },
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    orderStatus: {
+      type: String,
+      enum: {
+        values: ["Processing", "Shipped", "Delivered"],
+        message: "please select a payment method",
+      },
+      default: "Processing",
+    },
+    deliveredAt: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Order", OrderSchema);
