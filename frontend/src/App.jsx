27222e3d394ddca6be7248092923fe814408e4f0
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-04-18 17:10:36 +0200

    installing tailwind css and shadcn ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
-    <h1 className="text-3xl bg-black text-white text-center py-6">
-      Hello World
-    </h1>
+    <>
+      <h1 className="text-3xl bg-black text-white text-center py-6">
+        Hello World
+      </h1>
+      <Button variant="secondary">Click me</Button>
+    </>
   );
 }
 
