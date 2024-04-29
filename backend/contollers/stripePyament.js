daf79bc23e5e083c100a326a3d604907b9e9fef4
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-04-29 17:47:15 +0300

    implement stripe webhook to accept payment with card

--- backend/contollers/stripePyament.js
+++ backend/contollers/stripePyament.js
@@ -1,5 +1,7 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import Stripe from "stripe";
+import Order from "../models/order.js";
+import Product from "../models/product.js";
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 export const stripeCheckoutSession = catchAsyncErrors(
   async (req, res, next) => {
@@ -51,3 +53,97 @@ export const stripeCheckoutSession = catchAsyncErrors(
     });
   }
 );
+
+const getOrderItems = async (line_items) => {
+  return new Promise((resolve, reject) => {
+    let cartItems = [];
+
+    line_items?.data?.forEach(async (item) => {
+      const product = await stripe.products.retrieve(item.price.product);
+      const productId = product.metadata.productId;
+
+      const findProduct = await Product.findById(productId);
+      if (!product) {
+        return next(
+          new ErrorHandler(`no product found with that id: ${id}`, 404)
+        );
+      }
+
+      findProduct.stock = findProduct.stock - item.quantity;
+
+      await findProduct.save({ validateBeforeSave: false });
+
+      cartItems.push({
+        product: productId,
+        name: product.name,
+        price: item.price.unit_amount_decimal / 100,
+        quantity: item.quantity,
+        image: product.images[0],
+      });
+
+      if (cartItems.length === line_items?.data?.length) {
+        resolve(cartItems);
+      }
+    });
+  });
+};
+
+// Create new order after payment   =>  /api/v1/webhook
+export const stripeWebhook = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const signature = req.headers["stripe-signature"];
+
+    const event = stripe.webhooks.constructEvent(
+      req.rawBody,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+
+      const line_items = await stripe.checkout.sessions.listLineItems(
+        session.id
+      );
+
+      const orderItems = await getOrderItems(line_items);
+      const user = session.client_reference_id;
+
+      const totalAmount = session.amount_total / 100;
+      const taxAmount = session.total_details.amount_tax / 100;
+      const shippingAmount = session.total_details.amount_shipping / 100;
+      const itemsPrice = session.metadata.itemsPrice;
+
+      const shippingInfo = {
+        address: session.metadata.address,
+        city: session.metadata.city,
+        phoneNo: session.metadata.phoneNo,
+        zipCode: session.metadata.zipCode,
+        country: session.metadata.country,
+      };
+
+      const paymentInfo = {
+        id: session.payment_intent,
+        status: session.payment_status,
+      };
+
+      const orderData = {
+        shippingInfo,
+        orderItems,
+        itemsPrice,
+        taxAmount,
+        shippingAmount,
+        totalAmount,
+        paymentInfo,
+        paymentMethod: "Card",
+        user,
+      };
+
+      await Order.create(orderData);
+
+      res.status(200).json({ success: true });
+    }
+  } catch (error) {
+    console.log("Error => ", error);
+  }
+});
--- backend/routes/stripe_checkout.js
+++ backend/routes/stripe_checkout.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { stripeCheckoutSession } from "../contollers/stripePyament.js";
+import {
+  stripeCheckoutSession,
+  stripeWebhook,
+} from "../contollers/stripePyament.js";
 import { isAuthenticated } from "../middlewares/isAuthenticated.js";
 
 const router = express.Router();
 
 router.post("/payment/stripe-checkout", isAuthenticated, stripeCheckoutSession);
+router.post("/webhook", stripeWebhook);
 
 export default router;
