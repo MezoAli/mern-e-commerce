6d828393ecd57537bede2070016c9225f74205b7
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-05-10 12:01:08 +0300

    add metaData to reviews page and updating messages

--- frontend/src/pages/admin/Reviews.jsx
+++ frontend/src/pages/admin/Reviews.jsx
@@ -1,4 +1,5 @@
 import ReviewsTable from "@/components/admin/ReviewsTable";
+import Metadata from "@/components/layout/Metadata";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -10,7 +11,6 @@ const Reviews = () => {
   const [productId, setProductId] = useState("");
   const [fetchReviews, { data, isError, error, isLoading }] =
     useLazyGetReviewsForProductQuery();
-  console.log(data);
   const handleSubmit = async (e) => {
     e.preventDefault();
     fetchReviews(productId);
@@ -35,32 +35,35 @@ const Reviews = () => {
     );
   }
   return (
-    <div className="flex flex-col gap-8 max-w-xl mx-auto">
-      <form
-        className="flex flex-col justify-start items-start gap-4"
-        onSubmit={handleSubmit}
-      >
-        <div className="flex flex-col justify-start items-start w-full gap-4">
-          <Label className="text-xl font-semibold">Product Id</Label>
-          <Input
-            type="text"
-            placeholder="please type product Id to view it's reviews"
-            value={productId}
-            onChange={(e) => setProductId(e.target.value)}
-          />
-        </div>
-        <Button variant="auth" className="block mx-auto" type="submit">
-          {isLoading ? "Fetching..." : "Fetch Reviews"}
-        </Button>
-      </form>
-      {data?.reviews?.length > 0 ? (
-        <ReviewsTable reviews={data?.reviews} productId={productId} />
-      ) : (
-        <p className="text-xl font-semibold capitalize text-center">
-          There is no reviews for that product
-        </p>
-      )}
-    </div>
+    <>
+      <Metadata title="Admin Reviews" />
+      <div className="flex flex-col gap-8 max-w-xl mx-auto">
+        <form
+          className="flex flex-col justify-start items-start gap-4"
+          onSubmit={handleSubmit}
+        >
+          <div className="flex flex-col justify-start items-start w-full gap-4">
+            <Label className="text-xl font-semibold">Product Id</Label>
+            <Input
+              type="text"
+              placeholder="please type product Id to view it's reviews"
+              value={productId}
+              onChange={(e) => setProductId(e.target.value)}
+            />
+          </div>
+          <Button variant="auth" className="block mx-auto" type="submit">
+            {isLoading ? "Fetching..." : "Fetch Reviews"}
+          </Button>
+        </form>
+        {data?.reviews?.length > 0 ? (
+          <ReviewsTable reviews={data?.reviews} productId={productId} />
+        ) : (
+          <p className="text-xl font-semibold capitalize text-center">
+            {data && "There is no reviews for that product"}
+          </p>
+        )}
+      </div>
+    </>
   );
 };
 
