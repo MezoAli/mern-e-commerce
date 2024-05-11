06c0fa0b87f965b1cdaeafb3e65132abdd20ca88
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-05-11 08:20:43 +0300

    testing deployement

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api": "https://mern-e-commerce-jton.onrender.com",
     },
   },
 });
